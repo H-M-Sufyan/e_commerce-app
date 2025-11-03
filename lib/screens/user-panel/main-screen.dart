3cb513acbf8db9bfaa01ea0fac424b703e95b44d
H-M-Sufyan <hmsufyan61@gmail.com>
2025-11-02 20:21:59 -0800

    App Constant Update

--- lib/screens/user-panel/main-screen.dart
+++ lib/screens/user-panel/main-screen.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/appConstant.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
@@ -6,7 +7,11 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: AppConstant.AppMainColor,
+        title: Text(AppConstant.appMainName),
+        centerTitle: true,
+      ),
     );
   }
 }
---
+++ lib/utils/appConstant.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppConstant{
+  static String appMainName = "Sufyan's Store";
+  static String appPoweredBy = "Powered By H.M.Sufyan";
+  static Color? AppMainColor = Colors.deepOrangeAccent[700];
+  static Color? AppSecondColor = Colors.deepOrangeAccent[900];
+  static Color? AppTextColor = const Color.fromARGB(255, 227, 227, 227);
+  static Color? AppStatusBarColor = const Color.fromARGB(255, 227, 227, 227);
+}
