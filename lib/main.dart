8b6785f4068e864415c019c1bbf90e4b79fceb46
H-M-Sufyan <hmsufyan61@gmail.com>
2025-11-03 21:30:34 -0800

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,24 @@
 import 'package:e_commerce/screens/auth-ui/signin.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'screens/user-panel/main-screen.dart';
 import 'package:e_commerce/screens/auth-ui/splashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
